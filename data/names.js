ddead1f71414be2eb80e80c15f7588383860ee6b
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-04-19 13:02:35 +0200

    create data folder with seed files for provinces, names, and products

---
+++ data/names.js
@@ -0,0 +1,2 @@
+// A list of names
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
---
+++ data/provinces.js
@@ -0,0 +1,2 @@
+// A list of provinces
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
