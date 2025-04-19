85e7843412235298d2bd5df5e5b5bbd6b1007faa
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-04-19 17:46:57 +0200

    Clean up files

--- utils/namesUtils.js
+++ utils/namesUtils.js
@@ -1,14 +1,16 @@
+// Import seed data from the names and provinces files which are found inside the data folder
 import {names} from '../data/names.js';
 import { provinces } from '../data/provinces.js';
 
 // 1. **ForEach Basics**: Use `forEach` to log each name and each province to the console. 
-export const namesArray = [...names];
+export const namesArray = [...names];  //Create a shallow copy of the names array
 
 // 3. **Name Lengths**: Create a new array using `map` that contains the length of each name.
-export const nameLengths = (names) => names.map(name => name.length);
+export const nameLengths = (names) => names.map(name => name.length); // Returns an array of name lengths
 
 // 6. **Finding 'S'**: Create a boolean array using `map` and `some` to determine if a name contains the letter 'S'.
 export const namesContainS = (names) =>
+//For each name, it converts the name to lowercase, splits it into an array of characters, and checks if 's' is present at least once
     names.map(name => name.toLowerCase().split('').some(char => char === 's'));
 
 // 7. **Creating Object Mapping**: Use `reduce` to transform the names array into an object mapping names to their respective provinces.
--- utils/provincesUtils.js
+++ utils/provincesUtils.js
@@ -1,10 +1,11 @@
+//Import seed data from utils/provinces.js file
 import { provinces } from '../data/provinces.js';
 
 // 1. **ForEach Basics**: Use `forEach` to log each name and each province to the console. Then, log each name with a matching province in the format "Name (Province)".
-export const provincesArray = [...provinces]; 
+export const provincesArray = [...provinces];  // Create a shallow copy of the provinces array
 
 // 2. **Uppercase Transformation**: Use `map` to create a new array of province names in all uppercase. Log the new array to the console
-export const provincesUpperCase = provinces.map(province => province.toUpperCase());
+export const provincesUpperCase = provinces.map(province => province.toUpperCase()); // Convert all province names to uppercase
 
 // 4. **Sorting**: Use `sort` to alphabetically sort the provinces. 
 export const provincesSort = [...provinces].sort(); // Create a sorted copy of the provinces array without mutating the original
